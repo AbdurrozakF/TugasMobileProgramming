3b29229abf452792ed96f8c579b073129b8879aa
AbdurrozakF <hathayfathoni@gmail.com>
2019-12-18 17:33:59 +0700

    Recyclerview App

---
+++ app/src/main/java/com/example/abdurrozakf/tugasbesarmopro/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.abdurrozakf.tugasbesarmopro
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("WrongConstant")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rv = findViewById<RecyclerView>(R.id.recyclerView1)
+        rv.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+        val users = ArrayList<User>()
+        users.add(User("Paul", "Mr"))
+        users.add(User("Jane", "Miss"))
+        users.add(User("John", "Dr"))
+        users.add(User("Amy", "Mrs"))
+        users.add(User("Paul", "Mr"))
+        users.add(User("Jane", "Miss"))
+        users.add(User("John", "Dr"))
+        users.add(User("Amy", "Mrs"))
+        users.add(User("Paul", "Mr"))
+        users.add(User("Jane", "Miss"))
+        users.add(User("John", "Dr"))
+        users.add(User("Amy", "Mrs"))
+        users.add(User("Paul", "Mr"))
+        users.add(User("Jane", "Miss"))
+        users.add(User("John", "Dr"))
+        users.add(User("Amy", "Mrs"))
+        users.add(User("Paul", "Mr"))
+        users.add(User("Jane", "Miss"))
+        users.add(User("John", "Dr"))
+        users.add(User("Amy", "Mrs"))
+        users.add(User("Paul", "Mr"))
+        users.add(User("Jane", "Miss"))
+        users.add(User("John", "Dr"))
+        users.add(User("Amy", "Mrs"))
+
+        var adapter = CustomAdapter(users)
+        rv.adapter = adapter
+    }
+}
---
+++ app/src/main/java/com/example/abdurrozakf/tugasbesarmopro/User.kt
@@ -0,0 +1,3 @@
+package com.example.abdurrozakf.tugasbesarmopro
+
+data class User(val name:String, val title:String)
